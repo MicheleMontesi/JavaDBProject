53635c9d2571b3561e022764ac5b28860298fb51
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-06-22 18:03:50 +0200

    added changing operations list

--- app/src/main/java/controllers/Controller.java
+++ app/src/main/java/controllers/Controller.java
@@ -1,14 +1,13 @@
 package controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller implements Initializable {
 
@@ -16,12 +15,31 @@ public class Controller implements Initializable {
     private ListView<String> myEntityList;
     @FXML
     private ListView<String> myOperationList;
-
-    private List<String> entities = new ArrayList<>(Arrays.asList("Dipendente", "Paziente", "Unità Operativa", "Farmaco"));
+    private List<String> defaultOperations = new ArrayList<>(Arrays.asList("Crea", "Elimina", "Visualizza"));
+    private Map<String, List<String>> operationsMap= new HashMap<>(
+            Map.of("Dipendente", new ArrayList<>(defaultOperations),
+                    "Paziente", new ArrayList<>(defaultOperations),
+                    "Unita' Operativa", new ArrayList<>(defaultOperations),
+                    "Farmaco", new ArrayList<>(defaultOperations))
+    );
     private String currentEntitySelection;
+
+    private void initOperationMap() {
+        operationsMap.get("Dipendente").addAll(Arrays.asList("Visualizza Turni dato un Nome", "esempio"));
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        myEntityList.getItems().addAll(entities);
+        this.initOperationMap();
+        myEntityList.getItems().addAll(operationsMap.keySet());
+        myEntityList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                currentEntitySelection = myEntityList.getSelectionModel().getSelectedItem();
+                System.out.println(currentEntitySelection);
+                myOperationList.getItems().clear();
+                myOperationList.getItems().addAll(operationsMap.get(currentEntitySelection));
+            }
+        });
     }
 }
