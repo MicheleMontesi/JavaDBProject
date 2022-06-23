f1b8869d249b59730c5cfdf8d3ab5968552f3c58
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-06-23 10:28:48 +0200

    Refactoring

--- app/src/main/java/JavaDBProject/App.java
+++ app/src/main/java/JavaDBProject/App.java
@@ -8,17 +8,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import utilities.getScreenDimension;
+import utilities.GetScreenDimension;
 
 public class App extends Application{
 
-    private final static int WIDTH = getScreenDimension.center().width * 70/100;
-    private final static int HEIGHT = getScreenDimension.center().height * 70/100;
+    private final static int WIDTH = GetScreenDimension.center().width * 70/100;
+    private final static int HEIGHT = GetScreenDimension.center().height * 70/100;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(ClassLoader.getSystemResource("DBApplication.fxml"));
-        primaryStage.setTitle("Prova");
+        primaryStage.setTitle("DataBase Cooperativa Sanitaria");
         primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
         primaryStage.show();
     }
