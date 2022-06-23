b672b5ed3837e53dda9703b3e0bd2acd1b47bd5b
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-06-23 16:34:51 +0200

    Added possible operations for entities

--- app/src/main/java/utilities/CreateOperationsMap.java
+++ app/src/main/java/utilities/CreateOperationsMap.java
@@ -18,9 +18,12 @@ public class CreateOperationsMap {
         entities.forEach(e -> permittedOp.put(e, new ArrayList<>(defaultOperations)));
 
         permittedOp.get("Dipendente").addAll(Arrays.asList("Visualizza Turni", "Visualizza Attestati",
-                "Visualizza Contratto"));
+                "Visualizza Contratto", "Visualizza Farmaci Somministrati"));
         permittedOp.get("Paziente").addAll(Arrays.asList("Visualizza Cartella Clinica",
                 "Visualizza Inizio/Fine Ospitazione", "Visualizza Terapia"));
+        permittedOp.get("Unita' Operativa").addAll(Arrays.asList("Visualizza Turni", "Visualizza Beni Strumentali",
+                "Visualizza Ospitazioni"));
+        permittedOp.get("Beni Strumentali").addAll(Arrays.asList("Visualizza Automezzi", "Visualizza Attrezzature"));
 
         return permittedOp;
     }
