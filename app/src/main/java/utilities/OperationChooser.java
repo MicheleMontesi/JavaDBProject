9b7fd1367a715e02bfa50b71d6ecbbb5e9da5e22
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-06-24 11:28:07 +0200

    Created utility class that loads the correct fields

---
+++ app/src/main/java/utilities/OperationChooser.java
@@ -0,0 +1,52 @@
+package utilities;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class OperationChooser {
+
+    private String currentEntitySelection;
+    private String currentOperationSelection;
+    private BorderPane contentPane;
+
+    public void choose() {
+        switch (currentEntitySelection) {
+            case "Dipendente":
+                switch (currentOperationSelection) {
+                    case "Crea" -> this.loadContent("CreateWorker");
+                    case "Ricerca Tramite Parametro" -> this.loadContent("WorkerSearchByParameter");
+                    default -> this.contentPane.getChildren().clear();
+                }
+                break;
+            default:
+                this.contentPane.getChildren().clear();
+                break;
+        }
+    }
+
+    private void loadContent(String file) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(ClassLoader.getSystemResource(String.valueOf(Paths.get("Scenes", currentEntitySelection, file + ".fxml"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        contentPane.setCenter(root);
+    }
+
+    public void setCurrentEntitySelection(String currentEntitySelection) {
+        this.currentEntitySelection = currentEntitySelection;
+    }
+
+    public void setCurrentOperationSelection(String currentOperationSelection) {
+        this.currentOperationSelection = currentOperationSelection;
+    }
+
+    public void setContentPane(BorderPane contentPane) {
+        this.contentPane = contentPane;
+    }
+}
