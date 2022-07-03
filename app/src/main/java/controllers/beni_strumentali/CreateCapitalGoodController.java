9a982cd49bdb0b2902408437a1f51c18cd7f9460
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-07-03 11:37:37 +0200

    creating capital goods creator

---
+++ app/src/main/java/controllers/beni_strumentali/CreateCapitalGoodController.java
@@ -0,0 +1,15 @@
+package controllers.beni_strumentali;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class CreateCapitalGoodController {
+    public TextField unitField, goodField, toolField, plateField;
+    public DatePicker purchasePicker, maintenancePicker, expirationPicker;
+    public CheckBox vehicleCheck;
+    public ChoiceBox<String> typeChoice;
+
+
+}
