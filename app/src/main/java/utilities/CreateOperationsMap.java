922b4e416caa5a38800323cf7556a17f126221a9
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-07-20 18:16:00 +0200

    made operation chooser improvements

--- app/src/main/java/utilities/CreateOperationsMap.java
+++ app/src/main/java/utilities/CreateOperationsMap.java
@@ -2,14 +2,35 @@ package utilities;
 
 import java.util.*;
 
+import static java.util.Map.entry;
+
 public class CreateOperationsMap {
 
-    private static final List<String> entities = new ArrayList<>(Arrays.asList("Dipendente", "Paziente", "Unita' Operativa",
+    protected static final List<String> entities = new ArrayList<>(Arrays.asList("Dipendente", "Paziente", "Unita' Operativa",
             "Farmaco", "Beni Strumentali", "Farmaco Terapia",
             "Terapia", "Cartella Clinica", "Ospitazione",
             "Tipologia Contratto", "Contratto Stipulato",
             "Attestato Acquisito", "Tipologia Attestato",
             "Turno", "Assumere Terapia"));
+
+    protected static final Map<String, String> entitiesMap = Map.ofEntries(
+            entry("Dipendente", "Worker"),
+            entry("Paziente", "Patient"),
+            entry("Unita' Operativa", "OpUnit"),
+            entry("Farmaco", "Drug"),
+            entry("Beni Strumentali", "CapitalGood"),
+            entry("Farmaco Terapia", "TherapyDrug"),
+            entry("Terapia", "Therapy"),
+            entry("Cartella Clinica", "MedicalRecords"),
+            entry("Ospitazione", "Hosting"),
+            entry("Tipologia Contratto", "ContractType"),
+            entry("Contratto Stipulato", "SignedContract"),
+            entry("Attestato Acquisito", "CertificateAcquired"),
+            entry("Tipologia Attestato", "CertificateType"),
+            entry("Turno", "Shift"),
+            entry("Assumere Terapia", "TakeTherapy")
+    );
+
     private static final List<String> defaultOperations = new ArrayList<>(Arrays.asList("Crea", "Aggiorna", "Elimina",
             "Visualizza", "Ricerca Tramite Codice"));
 
--- app/src/main/java/utilities/OperationChooser.java
+++ app/src/main/java/utilities/OperationChooser.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.text.Normalizer;
 
+import static utilities.CreateOperationsMap.entitiesMap;
+
 public class OperationChooser {
 
     private String currentEntitySelection;
@@ -15,151 +17,21 @@ public class OperationChooser {
     private BorderPane contentPane;
 
     public void choose() {
-        try {
-            switch (currentEntitySelection) {
-                case "Dipendente":
+        for (var choice : CreateOperationsMap.entities) {
+            try {
+                if (currentEntitySelection.equals(choice)) {
                     switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateWorker");
-                        case "Aggiorna" -> this.loadContent("UpdateWorker");
-                        case "Elimina" -> this.loadContent("DeleteWorker");
-                        case "Visualizza" -> this.loadContent("WorkersView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("WorkerSearchByFiscalCode");
+                        case "Crea" -> this.loadContent("Create" + entitiesMap.get(currentEntitySelection));
+                        case "Aggiorna" -> this.loadContent("Update" + entitiesMap.get(currentEntitySelection));
+                        case "Elimina" -> this.loadContent("Delete" + entitiesMap.get(currentEntitySelection));
+                        case "Visualizza" -> this.loadContent("View" + entitiesMap.get(currentEntitySelection));
+                        case "Ricerca Tramite Codice" -> this.loadContent("Search" + entitiesMap.get(currentEntitySelection));
                         default -> this.contentPane.getChildren().clear();
                     }
-                    break;
-                case "Paziente":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreatePatient");
-                        case "Aggiorna" -> this.loadContent("UpdatePatient");
-                        case "Elimina" -> this.loadContent("DeletePatient");
-                        case "Visualizza" -> this.loadContent("PatientsView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("PatientSearchByFiscalCode");
-                    }
-                    break;
-                case "Turno":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateShift");
-                        case "Aggiorna" -> this.loadContent("UpdateShift");
-                        case "Elimina" -> this.loadContent("DeleteShift");
-                        case "Visualizza" -> this.loadContent("ShiftsView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("ShiftSearchByCode");
-                    }
-                    break;
-                case "Unita' Operativa":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateOpUnit");
-                        case "Aggiorna" -> this.loadContent("UpdateOpUnit");
-                        case "Elimina" -> this.loadContent("DeleteOpUnit");
-                        case "Visualizza" -> this.loadContent("OpUnitView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("OpUnitSearchByCode");
-                    }
-                    break;
-                case "Tipologia Attestato":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateCertificateType");
-                        case "Aggiorna" -> this.loadContent("UpdateCertificateType");
-                        case "Elimina" -> this.loadContent("DeleteCertificateType");
-                        case "Visualizza" -> this.loadContent("CertificateTypeView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("CertificateTypeSearchCode");
-                    }
-                    break;
-                case "Attestato Acquisito":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateCertificateAcquired");
-                        case "Aggiorna" -> this.loadContent("UpdateCertificateAcquired");
-                        case "Elimina" -> this.loadContent("DeleteCertificateAcquired");
-                        case "Visualizza" -> this.loadContent("CertificateAcquiredView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("SearchCertificatesAcquiredByCode");
-                    }
-                    break;
-                case "Tipologia Contratto":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateContractType");
-                        case "Aggiorna" -> this.loadContent("UpdateContractType");
-                        case "Elimina" -> this.loadContent("DeleteContractType");
-                        case "Visualizza" -> this.loadContent("ContractTypeView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("ContractTypeSearchCode");
-                    }
-                    break;
-                case "Contratto Stipulato":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateSignedContract");
-                        case "Aggiorna" -> this.loadContent("UpdateSignedContract");
-                        case "Elimina" -> this.loadContent("DeleteSignedContract");
-                        case "Visualizza" -> this.loadContent("SignedContractsView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("SearchSignedContractByCode");
-                    }
-                    break;
-                case "Farmaco":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateDrug");
-                        case "Aggiorna" -> this.loadContent("UpdateDrug");
-                        case "Elimina" -> this.loadContent("DeleteDrug");
-                        case "Visualizza" -> this.loadContent("DrugsView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("DrugSearchByCode");
-                    }
-                    break;
-                case "Terapia":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateTherapy");
-                        case "Aggiorna" -> this.loadContent("UpdateTherapy");
-                        case "Elimina" -> this.loadContent("DeleteTherapy");
-                        case "Visualizza" -> this.loadContent("TherapyView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("TherapySearchCode");
-                    }
-                    break;
-                case "Farmaco Terapia":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateTherapyDrugs");
-                        case "Aggiorna" -> this.loadContent("UpdateTherapyDrugs");
-                        case "Elimina" -> this.loadContent("DeleteTherapyDrug");
-                        case "Visualizza" -> this.loadContent("TherapyDrugView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("TherapyDrugByCode");
-                    }
-                    break;
-                case "Beni Strumentali":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateCapitalGood");
-                        case "Aggiorna" -> this.loadContent("UpdateCapitalGood");
-                        case "Elimina" -> this.loadContent("DeleteCapitalGood");
-                        case "Visualizza" -> this.loadContent("CapitalGoodsView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("SearchGoodByCode");
-                    }
-                    break;
-                case "Cartella Clinica":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateMedicalRecords");
-                        case "Aggiorna" -> this.loadContent("UpdateMedicalRecords");
-                        case "Elimina" -> this.loadContent("DeleteMedicalRecord");
-                        case "Visualizza" -> this.loadContent("MedicalRecordsView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("MedicalRecordSearchByCode");
-                    }
-                    break;
-                case "Ospitazione":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateHosting");
-                        case "Aggiorna" -> this.loadContent("UpdateHosting");
-                        case "Elimina" -> this.loadContent("DeleteHosting");
-                        case "Visualizza" -> this.loadContent("HostingView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("SearchHostingByCode");
-                    }
-                    break;
-                case "Assumere Terapia":
-                    switch (currentOperationSelection) {
-                        case "Crea" -> this.loadContent("CreateTakeTherapy");
-                        case "Aggiorna" -> this.loadContent("UpdateTakeTherapy");
-                        case "Elimina" -> this.loadContent("DeleteTakeTherapy");
-                        case "Visualizza" -> this.loadContent("TakeTherapyView");
-                        case "Ricerca Tramite Codice" -> this.loadContent("TakeTherapySearchCode");
-                    }
-                    break;
-                default:
-                    this.contentPane.getChildren().clear();
-                    break;
+                }
+            } catch (NullPointerException e) {
+                this.contentPane.getChildren().clear();
             }
-
-        } catch (NullPointerException e) {
-            this.contentPane.getChildren().clear();
         }
     }
 
