1421eba153d41abfa25002d4472a075c5a323723
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-07-24 11:38:52 +0200

    created method  getList

--- app/src/main/java/utilities/FillUtils.java
+++ app/src/main/java/utilities/FillUtils.java
@@ -1,10 +1,13 @@
 package utilities;
 
+import db.Table;
 import db.tables.CapitalGoodsTables;
 import javafx.scene.control.ChoiceBox;
 import model.CapitalGood;
+import model.Entity;
 
 import java.util.Objects;
+import java.util.function.Function;
 
 public class FillUtils {
     public static void fillGoodField(ChoiceBox<String> unitBox, ChoiceBox<String> goodBox, CapitalGoodsTables cgTable) {
@@ -15,6 +18,12 @@ public class FillUtils {
                         .filter(e -> e.unitId().equals(unitBox.getValue()))
                         .map(CapitalGood::goodId).map(Objects::toString).toList());
             }
+        }
+    }
+
+    public static void getList(ChoiceBox<String> box, Table<? extends Entity, String> table, Function<Entity, String> function) {
+        if (box != null) {
+            box.getItems().addAll(table.findAll().stream().map(function).toList());
         }
     }
 }
