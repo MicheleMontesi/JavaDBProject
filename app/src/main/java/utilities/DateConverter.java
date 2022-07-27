d44fcbb5e3c52a2332932f9c1a17003fb4d48e1b
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-07-27 13:52:41 +0200

    removed unused methods

--- app/src/main/java/utilities/DateConverter.java
+++ app/src/main/java/utilities/DateConverter.java
@@ -1,32 +1,12 @@
 package utilities;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Optional;
 
 public class DateConverter {
 
-    public static java.util.Date sqlDateToDate(final java.sql.Date sqlDate) {
-        return sqlDate == null ? null : new java.util.Date(sqlDate.getTime());
-    }
-
     public static java.sql.Date dateToSqlDate(final java.util.Date date) {
         return date == null ? null : new java.sql.Date(date.getTime());
-    }
-
-    public static Optional<Date> buildDate(final int day, final int month, final int year) {
-        try {
-            final String dateFormatString = "dd/MM/yyyy";
-            final String dateString = day + "/" + month + "/" + year;
-            final java.util.Date date = new SimpleDateFormat(dateFormatString, Locale.ITALIAN).parse(dateString);
-            return Optional.of(date);
-        } catch (final ParseException e) {
-            return Optional.empty();
-        }
     }
 
     public static LocalDate dateToLocalDate(String date) {
